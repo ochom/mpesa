1c0ae452aaaa8e91b6edd6f2457a6619cc9ae86e
ochom <ochomrichard752@gmail.com>
2024-06-19 10:18:30 +0300

    correct time stamp in migrations

--- src/models/b2c.go
+++ src/models/b2c.go
@@ -5,26 +5,27 @@ import (
 
 	"github.com/ochom/gutils/helpers"
 	"github.com/ochom/gutils/uuid"
+	"gorm.io/gorm"
 )
 
 // BusinessPayment store data when a business makes payment to Customer
 type BusinessPayment struct {
-	Id                       string    `json:"id"`
-	Amount                   string    `json:"amount"`
-	PhoneNumber              string    `json:"phone_number"`
-	RequestId                string    `json:"request_id"`
-	CallbackUrl              string    `json:"callback_url"`
-	ConversationID           string    `json:"conversation_id"`
-	OriginatorConversationID string    `json:"originator_conversation_id"`
-	TransactionID            string    `json:"transaction_id"`
-	ResponseCode             string    `json:"response_code"`
-	ResponseDescription      string    `json:"response_description"`
-	ResultCode               int       `json:"result_code"`
-	ResultDescription        string    `json:"result_description"`
-	Meta                     MetaData  `json:"meta" gorm:"type:json"`
-	CreatedAt                time.Time `json:"created_at"`
-	UpdatedAt                time.Time `json:"updated_at"`
-	DeletedAt                time.Time `json:"deleted_at"`
+	Id                       string         `json:"id"`
+	Amount                   string         `json:"amount"`
+	PhoneNumber              string         `json:"phone_number"`
+	RequestId                string         `json:"request_id"`
+	CallbackUrl              string         `json:"callback_url"`
+	ConversationID           string         `json:"conversation_id"`
+	OriginatorConversationID string         `json:"originator_conversation_id"`
+	TransactionID            string         `json:"transaction_id"`
+	ResponseCode             string         `json:"response_code"`
+	ResponseDescription      string         `json:"response_description"`
+	ResultCode               int            `json:"result_code"`
+	ResultDescription        string         `json:"result_description"`
+	Meta                     MetaData       `json:"meta" gorm:"type:json"`
+	CreatedAt                time.Time      `json:"created_at"`
+	UpdatedAt                time.Time      `json:"updated_at"`
+	DeletedAt                gorm.DeletedAt `json:"deleted_at,omitempty"`
 }
 
 // NewBusinessPayment create a new BusinessPayment
--- src/models/tax.go
+++ src/models/tax.go
@@ -4,27 +4,28 @@ import (
 	"time"
 
 	"github.com/ochom/gutils/uuid"
+	"gorm.io/gorm"
 )
 
 // TaxPayment store data when business makes a tax payment
 type TaxPayment struct {
-	Id                       string    `json:"id"`
-	Amount                   string    `json:"amount"`
-	ShortCode                string    `json:"short_code"`
-	PaymentRequestNumber     string    `json:"payment_request_number"` // prn
-	RequestId                string    `json:"request_id"`
-	CallbackUrl              string    `json:"callback_url"`
-	ConversationID           string    `json:"conversation_id"`
-	OriginatorConversationID string    `json:"originator_conversation_id"`
-	TransactionID            string    `json:"transaction_id"`
-	ResponseCode             string    `json:"response_code"`
-	ResponseDescription      string    `json:"response_description"`
-	ResultCode               int       `json:"result_code"`
-	ResultDescription        string    `json:"result_description"`
-	Meta                     MetaData  `json:"meta" gorm:"type:json"`
-	CreatedAt                time.Time `json:"created_at"`
-	UpdatedAt                time.Time `json:"updated_at"`
-	DeletedAt                time.Time `json:"deleted_at"`
+	Id                       string         `json:"id"`
+	Amount                   string         `json:"amount"`
+	ShortCode                string         `json:"short_code"`
+	PaymentRequestNumber     string         `json:"payment_request_number"` // prn
+	RequestId                string         `json:"request_id"`
+	CallbackUrl              string         `json:"callback_url"`
+	ConversationID           string         `json:"conversation_id"`
+	OriginatorConversationID string         `json:"originator_conversation_id"`
+	TransactionID            string         `json:"transaction_id"`
+	ResponseCode             string         `json:"response_code"`
+	ResponseDescription      string         `json:"response_description"`
+	ResultCode               int            `json:"result_code"`
+	ResultDescription        string         `json:"result_description"`
+	Meta                     MetaData       `json:"meta" gorm:"type:json"`
+	CreatedAt                time.Time      `json:"created_at"`
+	UpdatedAt                time.Time      `json:"updated_at"`
+	DeletedAt                gorm.DeletedAt `json:"deleted_at,omitempty"`
 }
 
 // NewTaxPayment create a new TaxPayment

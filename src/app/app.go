880d25a082d4800d53183a718e74a86c3b6f2dbd
ochom <ochomrichard752@gmail.com>
2024-06-19 15:57:04 +0300

    whitelist saf origins

--- src/app/app.go
+++ src/app/app.go
@@ -21,9 +21,9 @@ func New() *fiber.App {
 	c2b := v1.Group("/c2b")
 	c2b.Post("/register-urls", handlers.HandleC2BRegisterUrls)
 	c2b.Post("/initiate", handlers.HandleStkPush)
-	c2b.Post("/result", handlers.HandleC2BResult)
-	c2b.Post("/rest/validate", handlers.HandleRestValidation)
-	c2b.Post("/rest/confirm", handlers.HandleRestConfirmation)
+	c2b.Post("/result", safOrigins(handlers.HandleC2BResult))
+	c2b.Post("/rest/validate", safOrigins(handlers.HandleRestValidation))
+	c2b.Post("/rest/confirm", safOrigins(handlers.HandleRestConfirmation))
 	c2b.Post("/soap/validate", handlers.HandleSoapValidation)
 	c2b.Post("/soap/confirm", handlers.HandleSoapConfirmation)
 
@@ -31,15 +31,15 @@ func New() *fiber.App {
 	b2c := v1.Group("b2c")
 	b2c.Use(b2cCors())
 	b2c.Post("/initiate", handlers.HandleInitiatePayment)
-	b2c.Post("/result", handlers.HandleB2CResult)
-	b2c.Post("/timeout", handlers.HandleB2cTimeout)
+	b2c.Post("/result", safOrigins(handlers.HandleB2CResult))
+	b2c.Post("/timeout", safOrigins(handlers.HandleB2cTimeout))
 
 	// tax ...
 	tax := v1.Group("/tax")
 	tax.Use(taxCors())
 	tax.Post("/initiate", handlers.HandleTaxRemittance)
-	tax.Post("/result", handlers.HandleTaxResult)
-	tax.Post("/timeout", handlers.HandleTaxTimeout)
+	tax.Post("/result", safOrigins(handlers.HandleTaxResult))
+	tax.Post("/timeout", safOrigins(handlers.HandleTaxTimeout))
 
 	return app
 }
--- src/app/cors.go
+++ src/app/cors.go
@@ -1,10 +1,40 @@
 package app
 
 import (
+	"slices"
+
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/ochom/mpesa/src/app/config"
 )
+
+func safOrigins(next fiber.Handler) fiber.Handler {
+	origins := []string{
+		"196.201.214.200",
+		"196.201.214.206",
+		"196.201.214.207",
+		"196.201.214.208",
+		"196.201.213.114",
+		"196.201.213.44",
+		"196.201.212.127",
+		"196.201.212.138",
+		"196.201.212.129",
+		"196.201.212.136",
+		"196.201.212.74",
+		"196.201.212.69",
+	}
+
+	return func(c fiber.Ctx) error {
+		origin := c.Get("Origin")
+		if slices.Contains(origins, origin) {
+			return next(c)
+		}
+
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"message": "forbidden",
+		})
+	}
+}
 
 func b2cCors() fiber.Handler {
 	crs := cors.ConfigDefault
